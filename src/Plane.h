aa8dddaadec8d13c79febe99bea1d0539e1eabdc
Igor Liberato <igcbsb@gmail.com>
2021-12-04 14:49:44 -0300

    Liberato - Added Plane attributes and declared methods

--- src/Plane.h
+++ src/Plane.h
@@ -6,10 +6,33 @@
 #define PROJ1_PLANE_H
 
 class Plane{
-    private String registration;
-    private String type;
-    private int capacity;
-    private List<Flight> flights;
+    private:
+    String registration;
+    String type;
+    int capacity;
+    List<Flight> flights;
+    Queue<PlaneService> pastServices;
+    Queue<PlaneService> futureServices;
+public:
+    Plane(String registration, String type, int capacity){};
+    void SetFlights(List<Flight> flights){};
+    void SetPastServices(Queue<PlaneService> pastServices){};
+    void SetFutureServices(Queue<PlaneService> futureServices){};
+    String GetRegistration() const;
+    String GetType() const;
+    int GetCapacity() const;
+    List<Flight> GetFlights() const;
+    Queue<PlaneService> GetPastServices() const;
+    Queue<PlaneService> GetFutureServices() const;
+    void UpdateRegistration(String registration);
+    void UpdateType(String type);
+    void UpdateCapacity(int capacity);
+    void UpdateFlights(List<Flight> flights);
+    void AddFlight(Flight flight);
+    void UpdatePastServices(Queue<PlaneService> pastServices);
+    void AddPastService(PlaneService service);
+    void UpdateFutureServices(Queue<PlaneService> futureServices);
+    void AddFutureService(PlaneService service);
 };
 
 #endif //PROJ1_PLANE_H
