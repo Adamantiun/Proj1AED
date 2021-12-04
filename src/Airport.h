e453ff2f12d1acbeb34ef2df260ac8b897efbc15
Igor Liberato <igcbsb@gmail.com>
2021-12-04 16:09:54 -0300

    Liberato - Corrected a few errors

--- src/Airport.h
+++ src/Airport.h
@@ -2,27 +2,29 @@
 // Created by Acer on 01/12/2021.
 //
 
-#include "Plane.h"
-#include "TTL.h"
-
-
-
 #ifndef PROJ1_AIRPORT_H
 #define PROJ1_AIRPORT_H
+
+#include <list>
+#include <string>
+#include "Plane.h"
+#include "TTL.h"
+#include "BST.h"
+
+using namespace std;
 
 class Airport{
 private:
     BST<TTL> transportPlaces;
-    List<Plane> planes;
+    list<Plane> planes;
 public:
-    Airport(BST<TTL> transportPlaces, List<Plane> planes);
+    Airport(BST<TTL> transportPlaces, list<Plane> planes);
     BST<TTL> GetTransportPlaces();
-    List<Plane> GetPlanes();
+    list<Plane> GetPlanes();
     void UpdateTransportPlaces(BST<TTL> transportPlaces);
-    void AddTransportPlace(TTL transportPlaces);
-    void UpdateAirport(List<Plane> planes);
-    void AddAirport(Plane planes);
+    void AddTransportPlace(TTL transportPlace);
+    void UpdatePlanes(list<Plane> planes);
+    void AddPlanes(Plane plane);
 };
-
 
 #endif //PROJ1_AIRPORT_H
